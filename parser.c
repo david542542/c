abc50d726f76e6ab6a88139573f91f9eb3677000
Ubuntu <ubuntu@ip-172-30-1-178.us-west-1.compute.internal>
2021-01-19 23:14:45 +0000

    updates

--- parser.c
+++ parser.c
@@ -1,5 +1,7 @@
 asdfasdfasfasdfasdffsasdfasdfasdfasdfi:redraw!
-asdfasdfasdfzzzasdfasdf:redraw!asdfasdfasdf
+asdfasdfasdfzz
+
+hello zasdfasdf:redraw!asdfasdfasdf
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
