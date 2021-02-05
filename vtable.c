01bea77df4e8b82f151fa5155556ace2a136c1e1
Ubuntu <ubuntu@ip-172-30-1-178.us-west-1.compute.internal>
2021-02-05 03:57:34 +0000

    updates

--- vtable.c
+++ vtable.c
@@ -1,29 +1,72 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-typedef struct Person Person;
-typedef struct Person_VTable {
-    void (*print) (Person* self);
-} Person_VTable;
+typedef struct VTable VTable;
 
-typedef struct Person {
-    int id;
+typedef struct Thing {
+    const VTable *vtable;
     char* name;
-    const Person_VTable *vtable;
-} Person;
+} Thing;
 
-void print_name(Person* person) {
-    printf("Hello %s\n", person->name);
+typedef struct VTable {
+    void (*print)   (Thing* self);
+} VTable;
+
+
+void print_hello(Thing *self) {printf("Hello, %s", self->name);}
+static const VTable thing_vtable = {
+    .print = print_hello
+};
+typedef struct Alien {
+    const VTable *vtable;
+    char* name;
+    // add one more function to vtable -- should that be a stand-alone? do a second 'subclass vtable'? etc.
+} Alien;
+
+void alien_function(void) {printf("Alien");}
+Alien* init_alien(void)
+{
+    Alien* alien = malloc(sizeof(Alien));
+    alien->vtable = &thing_vtable;
+    /* alien->vtable->alien_function = alien_function; */
+    return alien;
 }
-Person* init_person(void) {
-    Person *person = malloc(sizeof(Person));
-    person->vtable  =  malloc(sizeof(Person_VTable));
-    person->vtable->print = print_name;
-    return person;
-}
+
 int main(void) {
-    Person* self = init_person();
-    self->name = "Greg";
-    self->vtable->print(self);
+    Alien *alien = init_alien();
+    /* alien->vtable->alien_function(); */
     return 0;
 }
+
+
+
+
+/* void increment_id(Person* person) { */
+    /* person->id = 4; // since this is pass-by-reference from the pointer, it will modify the values in place */
+    /* printf("Ok!\n"); */
+/* } */
+/* void print_name(Person* person) { */
+    /* printf("Hello %s (ID: %d)\n", person->name, person->id); */
+/* } */
+/* static const Person_VTable vtable1 = { */
+    /* .print = print_name, */
+    /* .adjust = increment_id */
+/* }; */
+/* Person* init_person( */
+        /* Person_VTable vtable */
+        /* [> void(*print)(Person*), <] */
+        /* [> void(*adjust)(Person*)  <] */
+/* ) { */
+    /* [> printf("Adjust: %p | Print: %p\n", adjust, print); <] */
+    /* Person *person = malloc(sizeof(Person)); */
+    /* person->vtable  =  &vtable1; */
+    /* return person; */
+/* } */
+/* int main(void) { */
+    /* Person* self = init_person(vtable1); */
+    /* self->name = "Greg"; */
+    /* self->vtable->print(self); */
+    /* self->vtable->adjust(self); */
+    /* self->vtable->print(self); */
+    /* return 0; */
+/* } */
